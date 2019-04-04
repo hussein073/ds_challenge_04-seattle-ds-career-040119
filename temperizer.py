a343e327fea8e666c4eda75d8135a008784fb3c3
hussein073 <48979163+hussein073@users.noreply.github.com>
2019-04-04 10:56:25 -0700

    DS CHallenge 4 WarmUp

--- temperizer.py
+++ temperizer.py
@@ -9,4 +9,23 @@ def convert_f_to_c(temperature_f):
 
 def convert_c_to_f(temperature_c):
     """Convert Celsius to Fahrenheit."""
-    pass
+    temperature_f = (9/5) * temperature_c + 32
+    return temperature_f
+
+def convert_c_to_k(temperature_c):
+    temperature_k = temperature_c + 273.15
+    return temperature_k
+
+def convert_f_to_k(temperature_f):
+    temperature_c = convert_f_to_c(temperature_f)
+    temperature_k = convert_c_to_k(temperature_c)
+    return temperature_k
+
+def convert_k_to_c(temperature_k):
+    temperature_c = temperature_k -273.15
+    return temperature_c
+
+def convert_k_to_f(temperature_k):
+    temperature_c = temperature_k -273.15
+    temperature_f = convert_c_to_f(temperature_c)
+    return temperature_f
